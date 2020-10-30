ac2cd068e9701f500d6f99c5f81258dd73570139
darkwebtourguide <accounts@chrisadams.io>
2020-10-30 14:15:16 -0400

    app layout/ firebase connection

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCYNntGxfimZpJAkKDJornx6QdS9LSon80",
+  authDomain: "chatway-6ef71.firebaseapp.com",
+  databaseURL: "https://chatway-6ef71.firebaseio.com",
+  projectId: "chatway-6ef71",
+  storageBucket: "chatway-6ef71.appspot.com",
+  messagingSenderId: "443574737481",
+  appId: "1:443574737481:web:ebb03288512d74abdd7d98"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
+
 
 function App() {
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
+      <section >
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
     </div>
   );
 }
+
+function SignIn() {
+  const singInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  }
+
+  return (
+    <button onClick={signInWithGoogle}>Sign in with Google</button>
+  )
+}
+
+function SignOut() {
+  return auth.currentUser && (
+
+    <button onClick={() => auth.signOut()}>Sign Out</button>
+  )
+}
+
+function ChatRoom() {
+
+  const messageRef = firestore.collection('messages');
+  const query = messageRef.orderBy('createAt');
+}
+
 
 export default App;
